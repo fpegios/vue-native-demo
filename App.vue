dfc51da21ac4a10362ae713a6beceb90a75b31ba
Fotis Pegios <9115341+fpegios@users.noreply.github.com>
2019-02-09 13:45:25 +0200

    button

--- App.vue
+++ App.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">{{ message }}</text>
+    <text class="text text-color-primary">{{ message }}</text>
+    <button v-bind:title="button" v-bind:on-press="handleBtnPress"/>
   </view>
 </template>
 
@@ -8,13 +9,23 @@
 export default {
   data: function() {
     return {
-      message: "Hello World"
+      message: "Hello World!",
+      button: "Button"
     };
+  },
+  methods: {
+    handleBtnPress: function() {
+      alert('Btn Press');
+    }
   }
 };
 </script>
 
 <style>
+  .text {
+    margin-bottom: 15px;
+  }
+
   .container {
     flex: 1;
     background-color: white;
